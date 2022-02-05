2ab2b60687175506d0580e9e84851418570d2287
mm-ig <inagaki@design.inf.shizuoka.ac.jp>
2022-02-06 00:51:21 +0900

    station14 done

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -13,7 +13,8 @@ function buildFullName(data) {
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
-    const fullname = getData().map(person => {
+    const dt = getData()
+    const fullname = dt.map(person => {
         const container = {};
         container.id = person.id;
         container.full_name = person.family_name +" "+ person.first_name;
